d998f2d15984aa9e784d52d7dd8b965d5b0c1d16
DharaniTS <124652991+DharaniTS@users.noreply.github.com>
2023-10-31 15:23:24 +0530

    Add files via upload

---
+++ SecondSum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class SecondSum
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter");
+float x=s.nextFloat();
+System.out.println(x);
+}
+}
