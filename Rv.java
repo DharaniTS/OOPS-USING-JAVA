a310c3bb9838a11b33447a491aa8bd44d30d2385
DharaniTS <124652991+DharaniTS@users.noreply.github.com>
2023-10-31 15:20:29 +0530

    Add files via upload

---
+++ Rv.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Rv
+{
+public static void main(String args[])
+{
+int i;
+System.out.println("Enter the number");
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+for(i=0;i<=n;i++)
+{
+System.out.println(i);
+}
+}
+}
